013c80779fad8c9373fb1673fa02d0c55aba1469
YuanSamuel <syuan@utexas.edu>
2025-02-27 11:52:04 -0600

    renaming

---
+++ easy/PalindromeNumber.java
@@ -0,0 +1,25 @@
+package easy;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(123));  
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int reversed = 0;
+        int temp = x;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp = temp / 10;
+        }
+
+        return x == reversed;
+    }
+}
